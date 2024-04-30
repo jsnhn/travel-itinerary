d8fa053e91a022a8385b3b89b3a51c0b11042323
jsnhn <jayhh122@gmail.com>
2024-04-30 11:12:14 -0700

    created and init .env

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('dotenv').config();
+
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/server');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
