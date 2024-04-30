6ea30e7566bc9d364a18ca82b6f3bcf026144290
jsnhn <jayhh122@gmail.com>
2024-04-30 16:23:31 -0700

    added tripSchema in trip model

--- models/trip.js
+++ models/trip.js
@@ -1,3 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema
 
+const tripSchema = new Schema({
+    location: {
+        type: String,
+        required: true,
+    },
+    startDate: {
+        type: Date,
+    },
+    endDate: {
+        type: Date,
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    flights: [flightSchema],
+    hotels: [hotelSchema],
+}, {
+    timestamps: true
+});
+module.exports = mongoose.model('Trip', tripSchema)
