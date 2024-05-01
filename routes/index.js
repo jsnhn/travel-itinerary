1ac3fc181b4fd7a7ae28e43880b63a9b3cb304ee
jsnhn <jayhh122@gmail.com>
2024-04-30 18:47:19 -0700

    Oauth authentication is functional

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/auth/google', passport.authenticate( // this method is going to return a middleware function that the router.get will use
+  'google',
+  {
+    scope:['profile', 'email'],
+    prompt: 'select_account' // if you only have one google, it will automatically log you in. wont take you to google
+  }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/', // if everything go rithe we go to trips index
+    failureRedirect: '/' // if they refuse to consent or lacking info, this is where you will be sent. could be the splash page
+  }
+));
+
+router.get('/logout', function(req, res){
+  req.logout(function() {
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
