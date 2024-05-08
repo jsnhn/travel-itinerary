18ab99f90019f11db2f69b7481e7814bb48f71df
jsnhn <jayhh122@gmail.com>
2024-05-08 09:01:10 -0700

    updates README.md

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/', indexRouter);
-app.use('/trips', tripsRouter);
+app.use('/trips', ensureLoggedIn, tripsRouter);
 app.use('/', ensureLoggedIn, excursionsRouter);
 
 // catch 404 and forward to error handler
