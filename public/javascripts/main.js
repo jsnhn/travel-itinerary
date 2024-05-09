09d4a1ac62ddd1ef1ce5f4c07c59b7165156f4da
jsnhn <jayhh122@gmail.com>
2024-05-08 19:24:11 -0700

    updates max date

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,21 +1,35 @@
-const startDateEl = document.querySelector('input[name="startDate"]')
+const startDateEl = document.querySelector('input[name="startDate"]') //selecting startDate attribute
+const endDateEl = document.querySelector('input[name="endDate"]');
 
 formatMinDate()
 
+startDateEl.addEventListener('change', formatMaxDate);
 
 
 function formatMinDate() {
-    const date = new Date()
-    const year = date.getFullYear()
-    let month = date.getMonth() + 1
-    let day = date.getDate()
+    const date = new Date();
+    const year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
     
-    if (month < 10) month = '0' + month
-    if (day < 10) day = '0' + day
-    startDateEl.setAttribute('min', `${year}-${month}-${day}`)
+    if (month < 10) month = '0' + month;
+    if (day < 10) day = '0' + day;
+    startDateEl.setAttribute('min', `${year}-${month}-${day}`);
 }
 
 
+    
+function formatMaxDate() {
+    const selectedStartDate = new Date(startDateEl.value);
+    const year = selectedStartDate.getFullYear();
+    let month = selectedStartDate.getMonth() + 1;
+    let day = selectedStartDate.getDate() + 1;
+    
+    if (month < 10) month = '0' + month;
+    if (day < 10) day = '0' + day;
+    
+    endDateEl.setAttribute('min', `${year}-${month}-${day}`);
+}
 
 //TODO select the start date input then you need to add an event listener to it everytime it changes. not a click a event. when that input changes you need to set the start date to be the min date of the end date.
 //TODO ... input end date. whatever i pick as the start date, the end date should not allow me to choose a date before the start date. 
